42e3f91870861c9ee14da2e3c0f29648d5ba1791
Dan Scannell <dscannell@randomhouse.com>
2013-08-11 17:47:17 -0400

    Added markdown, add, edit; improved delete

--- server-app/server-app.js
+++ server-app/server-app.js
@@ -1,5 +1,8 @@
-/* --- app setup --- */
-
+/*
+ * Express App Setup
+ *
+ * Launch the app & establish middleware
+ */
 var express = require('express');
 var app = express();
 app.listen(process.env.PORT || 8000);
@@ -9,13 +12,20 @@ app.use(function(req,res,next){
 	next();
 });
 
-/* --- mongolab --- */
+/*
+ * Mongo URI
+ * 
+ * Get URI from environment variables or fail
+ */
+var mongoURI = process.env.MONGOLAB_URI || process.env.LOCAL_MONGO_URI  || null;
+if (mongoURI == null) throw new Error('Mongo URI environment variable not set up');
 
-var mongoURI = process.env.MONGO_LAB_URI || process.env.MONGOLAB_URI || null;
-if (mongoURI == null) throw new Error('mongo uri env var not set up');
-
-/* --- mongoose --- */
-
+/*
+ * Mongo Connect
+ * 
+ * The moment of truth. Many tears have been
+ * shed trying to get that elusive connection.
+ */
 var mongoose = require('mongoose');
 mongoose.connect(mongoURI);
 var db = mongoose.connection;
@@ -26,6 +36,18 @@ db.once('open', function(){
 	isConnected = true;
 });
 
+/*
+ * Mongoose Models
+ *
+ * Models form the basis for all
+ * database interactions in the
+ * mongoose library.
+ *
+ * If the database doesn't seem to be
+ * updating properly, double check the
+ * properties in the schema for the
+ * troublesome model.
+ */
 var noteSchema = mongoose.Schema({
 	'name': String,
 	'markdown': String,
@@ -33,14 +55,10 @@ var noteSchema = mongoose.Schema({
 });
 var Note = mongoose.model('Note', noteSchema);
 
-/* --- data endpoints --- */
-
-app.get('/notes', function(req, res) {
-	
-	getAllNotes(req, res, 1);
-});
-
-app.post('/note', function(req, res){
+/*
+ * Update a single note
+ */
+function updateNote(req, res) {
 	console.log('app.post');
 	console.log(req.body);
 	var noteId = req.body._id;
@@ -54,9 +72,30 @@ app.post('/note', function(req, res){
 			res.send(500);
 		}
 	});
-});
+}
 
 /*
+ * Add a single note
+ */
+function addNote(req, res) {
+	console.log('addNote');
+	console.log(req.body);
+	var newNote = new Note(req.body);
+	newNote.save(function(err, note) {
+		if (!err) {
+			console.log('successfully updated');
+			console.log(note);
+			res.status(200);
+			res.send(note);
+		} else {
+			console.log(err);
+			res.send(500);
+		}
+	});
+}
+/*
+ * Fetch all non-deleted notes
+ * 
  * This function will recurse up to five
  * times if the db connection is not open.
  */
@@ -64,6 +103,8 @@ function getAllNotes(req, res, attempts) {
 	if (isConnected) {
 		Note.find({'deleted':false}, function(err, notes) {
 			if(!err) {
+				console.log('Retrieved ' + notes.length + ' notes from mongo');
+				console.log(notes);
 				res.send(notes);
 			} else {
 				res.status(501);
@@ -80,6 +121,20 @@ function getAllNotes(req, res, attempts) {
 		res.send({'error':'Failed to connect to database.'});
 	}
 }
+/* --- data endpoints --- */
+
+app.get('/notes', function(req, res) {
+	getAllNotes(req, res, 1);
+});
+
+app.post('/note', function(req, res){
+	if ( '_id' in req.body ) {
+		updateNote(req, res);
+	} else {
+		addNote(req, res);
+	}
+});
+
 
 app.get('/testnotes', function(req, res) {
 	var testData = [
