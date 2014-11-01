f10f1c8aef56a4b340e4117ea452e0f325430d10
drscannellrh <dscannell@randomhouse.com>
2014-11-01 16:29:26 -0400

    asynchronize mongo lookups

--- server/config/passport.js
+++ server/config/passport.js
@@ -27,26 +27,26 @@ module.exports = function(passport, User) {
 			passReqToCallback : true
 		},
 		function(req, email, password, done) {
-			User.findOne({ 'local.email' :  email }, function(err, user) {
-				if (err) {
-					return done(err);
-				}
-				if (user) {
-					return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-				} else {
-					var newUser= new User();
+			process.nextTick(function() {
+				User.findOne({ 'local.email' :  email }, function(err, user) {
+					if (err) {
+						return done(err);
+					}
+					if (user) {
+						return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+					} else {
+						var newUser= new User();
+						newUser.local.email= email;
+						newUser.local.password = newUser.generateHash(password);
+						newUser.save(function(err) {
+							if (err)
+							throw err;
+							return done(null, newUser);
+						});
+					}
 
-					newUser.local.email= email;
-					newUser.local.password = newUser.generateHash(password);
-					newUser.save(function(err) {
-						if (err)
-						throw err;
-						return done(null, newUser);
-					});
-				}
-
-			});
-
+				});
+		});
 	}));
 
 	passport.use('local-login', new LocalStrategy({
@@ -55,14 +55,16 @@ module.exports = function(passport, User) {
 			passReqToCallback : true
 		},
 		function(req, email, password, done) {
-			User.findOne({ 'local.email' :  email }, function(err, user) {
-				if (err)
-					return done(err);
-				if (!user)
-					return done(null, false, req.flash('loginMessage', 'No user found.'));
-				if (!user.validPassword(password))
-					return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-			return done(null, user);
+			process.nextTick(function() {
+				User.findOne({ 'local.email' :  email }, function(err, user) {
+					if (err)
+						return done(err);
+					if (!user)
+						return done(null, false, req.flash('loginMessage', 'No user found.'));
+					if (!user.validPassword(password))
+						return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+				return done(null, user);
+				});
 			});
 	}));
 
